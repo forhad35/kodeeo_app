e1cb1a36f476deabfb445786de2495f652502e45
sajusun <sajuislam266@gmail.com>
2023-10-31 15:46:29 +0600

    update saju branch data

--- lib/data/model/photos_link.dart
+++ lib/data/model/photos_link.dart
@@ -5,6 +5,9 @@ class PhotosLink{
     "https://www.kodeeo.com/upload/gallery/gallery_60e15041d4dab0.97341592.IMG_5060.webp",
     "https://www.kodeeo.com/upload/gallery/gallery_60e14fffaf5479.71168203.IMG_4942.webp",
     "https://www.kodeeo.com/upload/gallery/gallery_60e14ff4816964.89322660.IMG_4934.webp",
+    "https://www.kodeeo.com/upload/gallery/gallery_60e15041d4dab0.97341592.IMG_5060.webp",
+
+    "https://www.kodeeo.com/upload/gallery/gallery_60e14ff4816964.89322660.IMG_4934.webp",
   ];
 
 }
