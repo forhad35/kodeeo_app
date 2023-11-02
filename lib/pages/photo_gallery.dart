631311a6cc15652c0936e1351be769618b61203d
sajusun <sajuislam266@gmail.com>
2023-11-02 16:13:08 +0600

    update photo gallery

--- lib/pages/photo_gallery.dart
+++ lib/pages/photo_gallery.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:kodeeo_app/data/model/photos_link.dart';
 import 'package:kodeeo_app/pages/home.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:full_screen_image/full_screen_image.dart';
+import 'package:photo_view/photo_view.dart';
 
 class PhotoGallery extends StatelessWidget {
    PhotoGallery({super.key});
@@ -19,38 +21,56 @@ class PhotoGallery extends StatelessWidget {
             StaggeredGridTile.count(
               crossAxisCellCount: 2,
               mainAxisCellCount: 2,
-              child: Image.network(PhotosLink.imgList[0],fit: BoxFit.cover,),
+              child: loadimg(PhotosLink.imgList[0]),
             ),
             StaggeredGridTile.count(
               crossAxisCellCount: 2,
               mainAxisCellCount: 2,
-              child: Image.network(PhotosLink.imgList[1],fit: BoxFit.cover,),
+              child: loadimg(PhotosLink.imgList[1]),
             ),
             StaggeredGridTile.count(
               crossAxisCellCount: 4,
-              mainAxisCellCount: 3,
-              child: Image.network(PhotosLink.imgList[2],fit: BoxFit.cover,),
+              mainAxisCellCount: 2,
+              child: loadimg(PhotosLink.imgList[2]),
             ),
             StaggeredGridTile.count(
-              crossAxisCellCount: 2,
+              crossAxisCellCount: 3,
               mainAxisCellCount: 2,
-              child: Image.network(PhotosLink.imgList[3],fit: BoxFit.cover,),
+              child: loadimg(PhotosLink.imgList[3]),
             ),
             StaggeredGridTile.count(
-              crossAxisCellCount: 2,
-              mainAxisCellCount: 2,
-              child: Image.network(PhotosLink.imgList[4],fit: BoxFit.cover,),
+              crossAxisCellCount: 1,
+              mainAxisCellCount: 1,
+              child: loadimg(PhotosLink.imgList[4]),
+            ),
+            StaggeredGridTile.count(
+              crossAxisCellCount: 1,
+              mainAxisCellCount: 1,
+              child: loadimg(PhotosLink.imgList[5]),
             ),
             StaggeredGridTile.count(
               crossAxisCellCount: 4,
-              mainAxisCellCount: 3,
-              child: Image.network(PhotosLink.imgList[5],fit: BoxFit.cover,),
+              mainAxisCellCount: 2,
+              child: loadimg(PhotosLink.imgList[0]),
             ),
+
           ],
         ),
       ),
     );
   }
-
+  // image load function
+Widget loadimg(String PhotosLink){
+  return  Padding(
+    padding: const EdgeInsets.all(5.0),
+    child: Image.network(PhotosLink,fit: BoxFit.cover,),
+  );
+}
+//testing zoomable image
+  Widget FullScreenWidget(){
+   return ClipRRect(
+   borderRadius: BorderRadius.circular(16),
+   child: loadimg(PhotosLink.imgList[0]),
+   );}
 
 }
