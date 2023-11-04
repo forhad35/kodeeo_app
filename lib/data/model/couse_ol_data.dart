ee5761fa12e004489fb692519e2aa2c5c3802789
forhad35 <forhadj@gmail.com>
2023-11-04 18:27:09 +0600

    backout load

---
+++ lib/data/model/couse_ol_data.dart
@@ -0,0 +1,9 @@
+class CourseOlData{
+   var data =[
+     {"id":1,"title":"Things you should know as a web developer.","desc":[
+       "HTML/CSS/JS", "git","Browser DevTools","API (Application progRAMMING iNTERface)","Authentication",
+       "MVC (Modal View Controller)","Programming Language"," Problem Solving And   Searching","DevOps"
+   ]
+     }
+   ];
+}
