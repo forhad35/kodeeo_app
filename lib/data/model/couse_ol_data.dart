2e5b8043f4fd8646390297f38c5eec3afbc815ac
forhad35 <forhadj@gmail.com>
2023-11-05 11:35:24 +0600

    update course outline and couse ol data page

--- lib/data/model/couse_ol_data.dart
+++ lib/data/model/couse_ol_data.dart
@@ -1,9 +1,47 @@
 class CourseOlData{
-   var data =[
-     {"id":1,"title":"Things you should know as a web developer.","desc":[
+   var outLineData =[
+     {"id":1,"title":"Things you should know as a web developer.","desc":
+     [
        "HTML/CSS/JS", "git","Browser DevTools","API (Application progRAMMING iNTERface)","Authentication",
        "MVC (Modal View Controller)","Programming Language"," Problem Solving And   Searching","DevOps"
-   ]
-     }
+    ],"displayFlag":false
+     },
+     {"id":1,"title":"Report preparation.","desc":
+     [
+       "HTML/CSS/JS", "git","Browser DevTools","API (Application progRAMMING iNTERface)","Authentication",
+       "MVC (Modal View Controller)","Programming Language"," Problem Solving And   Searching","DevOps"
+     ],"displayFlag":false
+     },
+     {"id":1,"title":"Project Process","desc":
+     [
+       "HTML/CSS/JS", "git","Browser DevTools","API (Application progRAMMING iNTERface)","Authentication",
+       "MVC (Modal View Controller)","Programming Language"," Problem Solving And   Searching","DevOps"
+     ],"displayFlag":false
+     },
+     {"id":1,"title":"Practical Programming","desc":
+     [
+       "HTML/CSS/JS", "git","Browser DevTools","API (Application progRAMMING iNTERface)","Authentication",
+       "MVC (Modal View Controller)","Programming Language"," Problem Solving And   Searching","DevOps"
+     ],"displayFlag":false
+     },
+     {"id":1,"title":"Things you should know as a web developer.","desc":
+     [
+       "HTML/CSS/JS", "git","Browser DevTools","API (Application progRAMMING iNTERface)","Authentication",
+       "MVC (Modal View Controller)","Programming Language"," Problem Solving And   Searching","DevOps"
+     ],"displayFlag":false
+     },
+     {"id":1,"title":"Things you should know as a web developer.","desc":
+     [
+       "HTML/CSS/JS", "git","Browser DevTools","API (Application progRAMMING iNTERface)","Authentication",
+       "MVC (Modal View Controller)","Programming Language"," Problem Solving And   Searching","DevOps"
+     ],"displayFlag":false
+     },
+     {"id":1,"title":"Things you should know as a web developer.","desc":
+     [
+       "HTML/CSS/JS", "git","Browser DevTools","API (Application progRAMMING iNTERface)","Authentication",
+       "MVC (Modal View Controller)","Programming Language"," Problem Solving And   Searching","DevOps"
+     ],"displayFlag":false
+     },
+
    ];
 }
--- lib/widgets/course_outline.dart
+++ lib/widgets/course_outline.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kodeeo_app/data/model/couse_ol_data.dart';
 
 class CourseOutline extends StatefulWidget {
   const CourseOutline({super.key});
@@ -8,8 +9,22 @@ class CourseOutline extends StatefulWidget {
 }
 
 class _CourseOutlineState extends State<CourseOutline> {
+  var item=CourseOlData().outLineData;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  Scaffold(
+      appBar:  AppBar( title:  Text("Course Outline"),),
+      body: Container(
+        child:
+        ListView.builder(
+          itemCount: item.length,
+            itemBuilder: (itemBuilder,index){
+
+          return ListTile(
+
+          );
+        }),
+      ),
+    );
   }
 }
