ec29e4e8d57e46842f47fe5e88347ac3f64e1ba3
sajusun <sajuislam266@gmail.com>
2023-11-07 15:33:11 +0600

    notice page update and about course

--- lib/widgets/about_our_course.dart
+++ lib/widgets/about_our_course.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:kodeeo_app/helper/display.dart';
 class AboutCourse extends StatelessWidget {
   final String title;
   final String img;
@@ -12,62 +13,77 @@ class AboutCourse extends StatelessWidget {
   });
   @override
   Widget build(BuildContext context) {
-    var height = MediaQuery
-        .of(context)
-        .size
-        .height;
-    var width = MediaQuery
-        .of(context)
-        .size
-        .width;
-    return SingleChildScrollView(
-        child: Column(
-            children:[
-              Padding(
-                padding: const EdgeInsets.only(top: 5.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    TextButton.icon(onPressed: (){
-                      print(MediaQuery.of(context).padding.top + kToolbarHeight);
-                    }, icon: Icon(Icons.download_sharp,size: 14,color: Colors.orangeAccent,),
-                        label: Text("Course Outline",style: TextStyle(color: Colors.orangeAccent),)),
-                    TextButton(onPressed: (){
-                      print(AppBar().preferredSize.height);
-                    }, child: Text("Enroll Now",style: TextStyle(color: Colors.orangeAccent),),)
-                  ],
+
+    return Column(
+      children: [
+        Container(
+          height: (displayHeight(context)/1.23)*0.10,
+          padding: const EdgeInsets.all(10.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ElevatedButton.icon(onPressed: (){
+              }, icon: Icon(Icons.download_sharp,size: 14,color: Colors.white,),
+                label: Text("Course Outline",style: TextStyle(color: Colors.white),),
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5)
+                    ),
+                    backgroundColor: Colors.orangeAccent
+                ),),
+              ElevatedButton(onPressed: (){
+                print(AppBar().preferredSize.height);
+              },
+                child: Text("Enroll Now",style: TextStyle(color: Colors.white),),
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5)
+                    ),
+                    backgroundColor: Colors.orangeAccent
+                ),)
+            ],
+          ),
+        ),
+        Container(
+          height: (displayHeight(context)/1.23)*0.90,
+          width: displayWidth(context)*0.92,
+
+          child: SingleChildScrollView(
+              child: Container(
+                child: Column(
+                    children:[
+                      Container(
+                margin: const EdgeInsets.only(top: 5,),
+                child:Text( title,style: const TextStyle(
+                    height: 1.3,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold
+                ))
+          ),
+                      Container(
+                        margin:EdgeInsets.only(top: 20),
+                        width: displayWidth(context)*0.90,
+                        height:displayHeight(context)*0.20,
+                        child: Image.network(
+                            fit: BoxFit.fitWidth,
+                            img),
+                      ),
+                      Container(
+                          margin: EdgeInsets.only(top: 20),
+                          child:Text( discribtuion,textAlign: TextAlign.justify,
+                              style: const TextStyle(
+                                height: 1.3,
+                                fontSize: 14,
+                              )
+                          )
+                      ),
+                    ]
                 ),
               ),
-        Container(
-        margin: const EdgeInsets.only(top: 5,left: 5,right: 5),
-        child:Text( title,style: const TextStyle(
-            height: 1.3,
-            fontSize: 18,
-            fontWeight: FontWeight.bold
-        ))
-    ),
-              Container(
-                margin:EdgeInsets.only(top: height*0.02),
-                width: width*0.90,
-                height:height*0.23,
-                child: Image.network(
-                    fit: BoxFit.fitWidth,
-                    img),
-              ),
 
-              Container(
-                  margin: EdgeInsets.only(top: height*0.02),
-                  width: width*0.92,
-                  child:Text( discribtuion,textAlign: TextAlign.justify,
-                      style: const TextStyle(
-                        height: 1.3,
-                        fontSize: 14,
-                      )
-                  )
-              ),
-            ]
+          ),
         ),
-
+      ],
     );
   }
 }
