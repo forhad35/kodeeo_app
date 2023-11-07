e60d86de9278b28cba4a4c568a190549d370a38b
sajusun <sajuislam266@gmail.com>
2023-11-07 13:34:48 +0600

    notice page update

--- lib/pages/notification.dart
+++ lib/pages/notification.dart
@@ -1,13 +1,10 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:kodeeo_app/core/string_methods.dart';
 import 'package:kodeeo_app/data/model/notification_data.dart';
 
-import '../core/string_methods.dart';
-import '../data/model/notification_data.dart';
-
+@immutable
 class NotificationPage extends StatefulWidget {
-  NotificationPage({Key? key}) : super(key: key);
+  const NotificationPage({Key? key}) : super(key: key);
 
 
   @override
@@ -19,40 +16,49 @@ class _State extends State<NotificationPage>{
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      appBar: AppBar(title: Text("Notice"),centerTitle: true,),
-      body: ListView.builder(
-          itemCount: items.length,
-          itemBuilder: (context,index){
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                ListTile(
-                  leading: CircleAvatar(
-                    backgroundColor: Colors.grey,
-                    child: Text((index+1).toString()),
-                  ),
-                  title: Text(items[index].title),
-                  subtitle: Text(getFixedString(items[index].description, 45)),
-                  trailing: Icon(Icons.keyboard_arrow_right,color: Colors.grey,),
-                  minVerticalPadding: 10,
-                  selectedColor: Colors.cyan,
-                  onTap: (){
-                    setState(() {
-                      items[index].flag =!items[index].flag;
+      appBar: AppBar(title:const Text("Notice"),centerTitle: true,),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SizedBox(
+          child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context,index){
+                return Card(
+                  color: Colors.white,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ListTile(
+                        dense: false,
+                        leading: CircleAvatar(
+                          backgroundColor: Colors.grey,
+                          child: Text((index+1).toString()),
+                        ),
+                        title: Text(items[index].title),
+                        subtitle: items[index].flag?null:Text(getFixedString(items[index].description, 45)),
+                        trailing: items[index].flag?const Icon(Icons.keyboard_arrow_down_sharp,color: Colors.grey,):const Icon(Icons.keyboard_arrow_right,color: Colors.grey,),
+                        minVerticalPadding: 10,
+                        selectedColor: Colors.cyan,
+                        onTap: (){
+                          setState(() {
+                            items[index].flag =!items[index].flag;
 
-                    });
-                  },
-                ),
-                Visibility(
-                  visible: items[index].flag,
-                  child: Container(
-                    child: Text(items[index].description,textAlign: TextAlign.center,),
+                          });
+                        },
+                      ),
+                      Visibility(
+                        visible: items[index].flag,
+                        child: Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Text(items[index].description,textAlign: TextAlign.justify,style:const TextStyle(color: Colors.blueGrey),),
+                        ),
+                      ),
+                    ],
                   ),
-                )
-
-              ],
-            );
-          }),
+                );
+              }),
+        ),
+      ),
     );
   }
 }
